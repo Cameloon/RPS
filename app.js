356dd7db0acfe1a3dc5685d32359cfdf653ee48e
Cameloon <168191180+Cameloon@users.noreply.github.com>
2024-09-02 21:22:35 +0200

    created function which, if called, returns a random string of either: rock, paper or scissors

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let rndmnmb = Math.random()
+    if(rndmnmb >= 0.6) {
+        return "Rock"
+    } else if (rndmnmb >= 0.3) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+    
+ 
+}
+
+console.log(getComputerChoice())
