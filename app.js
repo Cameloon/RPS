9a3c7c3ae8da545b38efb1ea265b1be61633ea47
Cameloon <168191180+Cameloon@users.noreply.github.com>
2024-09-03 20:59:37 +0200

    implemented function to get user input either: rock, paper or scissors and added a function which determines who has won

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 function getComputerChoice() {
     let rndmnmb = Math.random()
-    if(rndmnmb >= 0.6) {
-        return "Rock"
+    if (rndmnmb >= 0.6) {
+        let rndmnmb = "rock"
+        console.log("Comp chose: " + rndmnmb)
+        return rndmnmb
     } else if (rndmnmb >= 0.3) {
-        return "Paper"
+        let rndmnmb = "paper"
+        console.log("Comp chose: " + rndmnmb)
+        return rndmnmb
     } else {
-        return "Scissors"
+        let rndmnmb = "scissors"
+        console.log("Comp chose: " + rndmnmb)
+        return rndmnmb
     }
-    
- 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let hChoice = window.prompt("Rock, Paper or Scissors?").toLocaleLowerCase();
+    console.log("You chose: " + hChoice)
+    return hChoice
+}
+
+function playRound() {
+    let humanSelect = getHumanChoice();
+    let compSelect = getComputerChoice();
+    
+    if(humanSelect == "rock" && compSelect =="scissors") {
+        console.log("You win")
+    }else if(humanSelect == "rock" && compSelect == "paper") {
+        console.log("You lose")
+    }else if(humanSelect == "paper" && compSelect == "rock") {
+        console.log("You win")
+    }else if(humanSelect == "paper" && compSelect == "scissors") {
+        console.log("You lose")
+    }else if(humanSelect == "scissors" && compSelect == "rock") {
+        console.log("You lose")
+    }else if(humanSelect == "scissors" && compSelect == "paper") { 
+        console.log("You win")
+    }else {
+        console.log("Tied")
+    }
+}
+
+
+playRound()
